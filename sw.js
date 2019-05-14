a15e6e190761cd42978c81be0116300b31f80601
Rafael Morales <rafaelmp@gmail.com>
2019-05-14 01:45:04 -0500

    [chore] Estilo mejorado al seleccionar personaje, se subio la version del cache dinamico así como la eliminacion de versiones viejas del cache dinamico

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 importScripts( 'js/sw-utils.js' );
 
 const STATIC_CACHE    = 'static-v2';
-const DYNAMIC_CACHE   = 'dynamic-v1';
+const DYNAMIC_CACHE   = 'dynamic-v2';
 const INMUTABLE_CACHE = 'inmutable-v1';
 
 const APP_SHELL = [
@@ -52,6 +52,10 @@ self.addEventListener( 'activate', e => {
             // static-v1
             if ( key !== STATIC_CACHE && key.includes( 'static' ) ) {
                 return caches.delete( key );
+            }
+
+            if (  key !== DYNAMIC_CACHE && key.includes('dynamic') ) {
+                return caches.delete(key);
             }
 
         });
